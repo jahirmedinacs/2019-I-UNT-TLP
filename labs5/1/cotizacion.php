71dec55a0de4721a43223a6a41e89cca812fd49b
DrSavage <services@jahirmedina.com>
2019-05-29 15:23:28 -0500

    v0.00.00.1111

---
+++ labs5/1/cotizacion.php
@@ -0,0 +1,89 @@
+<?php
+
+$servername = "localhost";
+$username = "tlp_query";
+$password = "1234";
+$dbname = "tlp_shop";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$productos = array();
+
+$sql = "SELECT * FROM inventario";
+$result = mysqli_query($conn, $sql);
+
+$sub_productos = array();
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        array_push($sub_productos, $row);
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+
+echo "<h1>Cotizacion</h1>";
+echo "<center>";
+echo "<ul>";
+
+$__temp = array_pop($_POST);
+
+$ref_keys = array_keys($_POST);
+
+$total = 0;
+for ($ii=0; $ii<count($_POST); $ii++){
+    $temp_ref = $ref_keys[$ii]; 
+    
+    if (($ii + 1) % 2 == 0){
+        $prod_amount = $_POST[$temp_ref];
+        $ref_id = str_replace("AMOUNT_", "", $temp_ref);
+        $ref_prod = $productos[$ref_id];
+        $ref_prec = $precios[$ref_id];
+
+        $sub_total = $ref_prec * $prod_amount;
+        
+        echo "<li>";
+        echo "<b>$prod_amount</b>\t\t<b>$ref_prod</b>'s
+            \ta\t<b>$ref_prec</b> la unidad, cuesta
+            \t<b>$sub_total</b>";
+        echo "</li>";
+
+        $total += $sub_total;
+    }
+}
+    echo "</ul>";
+echo "</center>";
+echo "<h2>";
+echo "</br></br><b>Total :\t $total Soles</b>";
+echo "</h2>";
+?>
+
+<!--
+echo var_dump($_POST);
+echo "\n\n";
+foreach ($_POST as $ref){
+    echo "$ref\n";
+} 
+-->
+<!--
+$amount = $_POST["TIMES"];
+$base = $_POST["BASE"];
+$factor = range(1, $amount);
+$output = [];
+
+foreach ($factor as $ii){
+    array_push($output, $base * $ii);
+}
+
+$rows_ref = $amount * 2 + 5;
+echo "<textarea rows=\"$rows_ref\" cols=\"30\">";
+echo "\n\n\n";
+echo var_dump($output);
+echo "</textarea>";
+
+-->
